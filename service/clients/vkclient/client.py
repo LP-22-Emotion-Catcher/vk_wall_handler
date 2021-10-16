c807434ecdaa1e22124bdcd1d05d310cbbbff1ec
Evgenij Karpov <gedrip87@gmail.com>
2021-10-16 17:10:55 +0300

    rewrite vkclient

--- service/clients/vkclient/client.py
+++ service/clients/vkclient/client.py
@@ -1,3 +1,6 @@
+from typing import Any
+from glom import glom
+import arrow
 import httpx
 
 from service.clients.vkclient.serializers import Post
@@ -23,5 +26,20 @@ class VKClient:
         response = httpx.get(self.url, params=data)
         response.raise_for_status()
 
-        posts = response.json()
-        return [Post(**post) for post in posts]
+        posts = response.json()['response']['items']
+        print(posts)
+        return [self._convert(post, owner_id) for post in posts]
+
+    def _convert(self, post: dict[str, Any], owner_id: int) -> Post:
+        post_id = glom(post, 'id', default=None)
+        return Post(
+            uid=int(post_id),
+            created=arrow.get(post['date']).datetime,
+            author_id=post['from_id'],
+            link=f'https://vk.com/wall{owner_id}_{post_id}',
+            likes=glom(post, 'likes.count', default=0),
+            reposts=glom(post, 'reposts.count', default=0),
+            comments=glom(post, 'comments.count', default=0),
+            views=glom(post, 'views.count', default=0),
+            text=glom(post, 'text', default=None),
+        )
