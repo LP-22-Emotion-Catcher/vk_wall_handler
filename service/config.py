adb243317ce0506be4d5feb3ca15c783ac836005
Evgenij Karpov <gedrip87@gmail.com>
2021-10-26 06:58:32 +0300

    update config file

--- service/config.py
+++ service/config.py
@@ -7,13 +7,6 @@ key = os.environ['SAFE_KEY']
 
 chunk_size = int(os.environ.get('CHUNK_SIZE', '100'))
 
-# Some examples of groups in VK (we need their ID)
-owner_id = int(os.environ['OWNER_ID'])
-
-pr_owner_id = os.environ['PR_OWNER_ID']
-py_owner_id = os.environ['PY_OWNER_ID']
-ro_owner_id = os.environ['RO_OWNER_ID']
-
-groups = os.environ['GROUPS'].split(',')
 
 backend_url = os.environ['BACKEND_URL']
+backend_config = os.environ['BACKEND_CONFIG']
