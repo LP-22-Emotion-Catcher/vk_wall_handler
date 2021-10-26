55438d48127d3e54dce539769026d22e30128d33
Evgenij Karpov <gedrip87@gmail.com>
2021-10-26 06:59:07 +0300

    update our worker, first view

--- service/worker.py
+++ service/worker.py
@@ -15,25 +15,22 @@ class Worker:
         self.backend = backend.BackClient(backend_url)
 
     def work(self) -> None:
-        wall = self.backend.get_walls()[0]
-        owner_id = wall.wall_id
+        walls = self.backend.get_walls()
+        for wall in walls:
+            owner_id = wall['wall_id']
+            last_post_id = wall['last_post_id']
 
-        time_delay = random.randrange(60, 360)
+            time_delay = random.randrange(60, 360)
 
-        last_post = self.vk.get_posts(owner_id, offset=0)[0]
-        saved_post = self._convert(last_post)
-        self.backend.send_post(saved_post)
-
-        while True:
-            time.sleep(time_delay)
-            new_post = self.vk.get_posts(owner_id, offset=0)[0]
-            if new_post.created > last_post.created:
-                last_post = new_post
-                saved_post = self._convert(last_post)
-                self.backend.send_post(saved_post)
-            else:
-                logger.info("There are no new messages")
-                continue
+            while True:
+                time.sleep(time_delay)
+                new_post = self.vk.get_posts(owner_id, offset=0)[0]
+                if new_post.uid > last_post_id:
+                    saved_post = self._convert(new_post)
+                    self.backend.send_post(saved_post)
+                else:
+                    logger.info("There are no new messages")
+                    continue
 
     def _convert(self, post: vkclient.Post) -> backend.Post:
         return backend.Post(
