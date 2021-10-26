b654faa778ed18b58fa5cefa8995e36292ea8c17
Evgenij Karpov <gedrip87@gmail.com>
2021-10-26 11:52:13 +0300

    new url for comments

--- service/config.py
+++ service/config.py
@@ -9,4 +9,5 @@ chunk_size = int(os.environ.get('CHUNK_SIZE', '100'))
 
 
 backend_url = os.environ['BACKEND_URL']
+backend_comments_url = os.environ['BACKEND_COMMENTS_URL']
 backend_config = os.environ['BACKEND_CONFIG']
