1d6803141ccaa2d2800987f544b7e1358b912a0e
Evgenij Karpov <gedrip87@gmail.com>
2021-10-26 06:58:01 +0300

    new method for getting config

--- service/clients/backend/client.py
+++ service/clients/backend/client.py
@@ -1,11 +1,10 @@
 import logging
 
-
 import httpx
 import orjson
 
-from service.clients.backend.serializers import Post, Wall
-from service.config import owner_id
+from service.clients.backend.serializers import Post
+from service.config import backend_config
 
 
 logger = logging.getLogger(__name__)
@@ -27,5 +26,12 @@ class BackClient:
         except httpx.ConnectError:
             logger.debug('can\'t send message due to connection problem')
 
-    def get_walls(self) -> list[Wall]:
-        return [Wall(wall_id=owner_id, last_post_id=-1)]
+    def get_walls(self):
+        try:
+            response = httpx.get(backend_config)
+            walls = response.json()
+            logger.debug('walls config has been recieved')
+        except httpx.ConnectError:
+            logger.debug('can\'t recieve walls config due to connection problem')
+
+        return walls
