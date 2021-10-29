aa35e8ccab0f5b7d118a9f3bf3fb0d0d9839ecb0
Evgenij Karpov <gedrip87@gmail.com>
2021-10-29 18:13:28 +0300

    refactoring

--- service/worker.py
+++ service/worker.py
@@ -19,21 +19,23 @@ class Worker:
         while True:
             walls = self.backend.get_walls()
             if not walls:
-                logger.debug('Can\'t receive a config from backend. I will try once again in 5 minutes')
+                logger.debug(f'Can\'t receive a config from backend {backend_url}.\
+                             I will try once again in 5 minutes')
                 time.sleep(self.delay)
-            else:
-                for wall in walls:
-                    owner_id = wall['wall_id']
-                    last_post_id = wall['last_post_id']
-                    new_post = self.vk.get_posts(owner_id, offset=0)[0]
-                    if new_post.uid <= last_post_id:
-                        logger.debug(f"There are no new messages on wall {owner_id}")
-                        continue
-                    saved_post = self._convert_post(new_post)
-                    self.backend.send_post(saved_post)
-                    time.sleep(random.randrange(3, 10))
-                logger.debug('I am waiting in 5 minutes to make a new query')
-                time.sleep(self.delay)
+                continue
+
+            for wall in walls:
+                owner_id = wall['wall_id']
+                last_post_id = wall['last_post_id']
+                new_post = self.vk.get_posts(owner_id, offset=0)[0]
+                if new_post.uid <= last_post_id:
+                    logger.debug(f"There are no new messages on wall {owner_id}")
+                    continue
+                saved_post = self._convert_post(new_post)
+                self.backend.send_post(saved_post)
+                time.sleep(random.randrange(3, 10))
+            logger.debug('I am waiting in 5 minutes to make a new query')
+            time.sleep(self.delay)
 
     def _convert_post(self, post: vkclient.Post) -> backend.Post:
         return backend.Post(
